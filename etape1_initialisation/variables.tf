f1a5daf571de28b55789be1a830ce3a0386fdaf5
Bastien <bastien.cadiot@cpexterne.org>
2017-05-19 01:21:59 +0200

    Change vars

--- etape1_initialisation/variables.tf
+++ etape1_initialisation/variables.tf
@@ -1,3 +1,11 @@
+variable "dist_user" {
+  description = "SSH User"
+}
+
+variable "private_key_path" {
+  default = "~/.ssh/id_rsa"
+}
+
 variable "region" {
   default = "europe-west1-d" // We're going to need it in several places in this config
 }
