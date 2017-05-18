dd6b233ce6ce8f6bf2309b8e9f440e3076d3d1ac
Bastien <bastien.cadiot@cpexterne.org>
2017-05-19 01:21:09 +0200

    Add provisionning

--- etape1_initialisation/servers.tf
+++ etape1_initialisation/servers.tf
@@ -19,4 +19,48 @@ resource "google_compute_instance" "servers" {
       // Auto generate
     }
   }
+
+  provisioner "file" {
+    connection {
+      user = "${var.dist_user}"
+      host = "${self.network_interface.0.access_config.0.assigned_nat_ip}"
+      timeout = "60s"
+      private_key = "${file("${var.private_key_path}")}"
+      agent = false
+    }
+
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user = "${var.dist_user}"
+      host = "${self.network_interface.0.access_config.0.assigned_nat_ip}"
+      timeout = "60s"
+      private_key = "${file("${var.private_key_path}")}"
+      agent = false
+    }
+
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo /tmp/bootstrap.sh server gce-west1"
+    ]
+  }
+}
+
+resource "null_resource" "servers_provisioning" {
+  provisioner "remote-exec" {
+    connection {
+      user = "${var.dist_user}"
+      host = "${element(google_compute_instance.servers.*.network_interface.0.access_config.0.assigned_nat_ip, 0)}"
+      timeout = "60s"
+      private_key = "${file("${var.private_key_path}")}"
+      agent = false
+    }
+
+    inline = [
+      "consul join ${join(" ", google_compute_instance.servers.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+    ]
+  }
 }
