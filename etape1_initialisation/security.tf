b78257b312a11af89b374f51e7980de1d0f22a26
Bastien <bastien.cadiot@cpexterne.org>
2017-05-19 10:21:06 +0200

    Fix firewall rules for servers

--- etape1_initialisation/security.tf
+++ etape1_initialisation/security.tf
@@ -1,5 +1,5 @@
-resource "google_compute_firewall" "nomad-icmp" {
-  name    = "nomad-icmp"
+resource "google_compute_firewall" "icmp" {
+  name    = "icmp"
   network = "${google_compute_network.nomad.name}"
 
   allow {
@@ -7,12 +7,48 @@ resource "google_compute_firewall" "nomad-icmp" {
   }
 }
 
-resource "google_compute_firewall" "nomad-ssh" {
-  name    = "nomad-ssh"
+resource "google_compute_firewall" "ssh" {
+  name    = "ssh"
   network = "${google_compute_network.nomad.name}"
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
+}
+
+resource "google_compute_firewall" "consul-servers" {
+  name    = "consul-servers"
+  network = "${google_compute_network.nomad.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8300-8302", "8500"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["8301-8302"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["consul-servers"]
+}
+
+resource "google_compute_firewall" "nomad-servers" {
+  name    = "nomad-servers"
+  network = "${google_compute_network.nomad.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["4646-4648"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["4648"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["nomad-servers"]
 }
