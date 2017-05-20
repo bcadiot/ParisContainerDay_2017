4e77c4a0a03961bf7f4b455041cc37c8f4ce687d
Bastien <bastien.cadiot@cpexterne.org>
2017-05-20 13:04:01 +0200

    Add provisioning part for clients

--- etape3_scaling/clients.tf
+++ etape3_scaling/clients.tf
@@ -1,6 +1,7 @@
 resource "openstack_compute_instance_v2" "data_node" {
+  count           = 0
   region          = "GRA3"
-  name            = "data_node"
+  name            = "client-ovh-france-${count.index + 1}"
   image_name      = "CentOS 7"
   flavor_name     = "s1-2"
   key_pair        = "Bastien-MBP"
@@ -8,5 +9,34 @@ resource "openstack_compute_instance_v2" "data_node" {
 
   network {
     name = "Ext-Net"
+  }
+
+  provisioner "file" {
+    connection {
+      user = "${var.dist_user}"
+      host = "${self.access_ip_v4}"
+      timeout = "60s"
+      private_key = "${file("${var.private_key_path}")}"
+      agent = false
+    }
+
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user = "${var.dist_user}"
+      host = "${self.access_ip_v4}"
+      timeout = "60s"
+      private_key = "${file("${var.private_key_path}")}"
+      agent = false
+    }
+
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo /tmp/bootstrap.sh client europe france",
+      "consul join ${join(" ", openstack_compute_instance_v2.servers.*.access_ip_v4)}"
+    ]
   }
 }
